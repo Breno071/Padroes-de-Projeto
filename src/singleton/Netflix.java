b7d199c634e7d4ffd2961c60c91c55005a93a1a5
Breno071 <brenonarde@gmail.com>
2021-09-14 11:14:44 -0300

    Singleton Eager

---
+++ src/singleton/Netflix.java
@@ -0,0 +1,26 @@
+package singleton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Netflix {
+    private final List<String> filmes = new ArrayList<>();
+    private static final Netflix NETFLIX = new Netflix();
+
+    public  void addFilme(String filme){
+        filmes.add(filme);
+    }
+
+    {
+        filmes.add("Star wars");
+        filmes.add("Avengers");
+    }
+
+    public List<String> getFilmes() {
+        return filmes;
+    }
+
+    public static Netflix getNetflix(){
+        return NETFLIX;
+    }
+}
---
+++ src/singleton/NetflixTest01.java
@@ -0,0 +1,16 @@
+package singleton;
+
+public class NetflixTest01 {
+    public static void main(String[] args) {
+
+        Netflix.getNetflix().addFilme("Batman");
+        System.out.println(Netflix.getNetflix().getFilmes());
+
+        Netflix netflix = Netflix.getNetflix();
+
+        //Podemos ver pelo endereço de memória que o objeto criado é o mesmo existente na classe Netflix
+        System.out.println(netflix);
+        System.out.println(Netflix.getNetflix());
+
+    }
+}
